f32a10124c8131f8223d1ec0d5776df300d900b6
Paul-Andrew Joseph Miseiko <pmiseiko@gmail.com>
2020-11-29 23:55:37 -0500

    Clean up hearth and handle when teleport fails.

--- src/main/java/ca/nightfury/minecraft/plugin/player/hearth/HearthCommandHandler.java
+++ src/main/java/ca/nightfury/minecraft/plugin/player/hearth/HearthCommandHandler.java
@@ -2,6 +2,7 @@ package ca.nightfury.minecraft.plugin.player.hearth;
 
 import org.bukkit.Location;
 import org.bukkit.Server;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -31,62 +32,83 @@ public class HearthCommandHandler implements CommandExecutor
     @Override
     public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args)
     {
-        final String commandName = command.getName().toLowerCase();
-        switch (commandName)
+        final String commandName = command.getName();
+        if (HEARTH_COMMAND_NAME.equalsIgnoreCase(commandName))
         {
-            case "hearth":
-                final String playerName = sender.getName();
-                final Player player = m_server.getPlayer(playerName);
-                final PlayerIdentity playerIdentity = new PlayerIdentity(player);
+            final String playerName = sender.getName();
+            final Player player = m_server.getPlayer(playerName);
+            final PlayerIdentity playerIdentity = new PlayerIdentity(player);
 
-                if (m_database.isHearthDisabled(playerIdentity))
+            if (m_database.isHearthDisabled(playerIdentity))
+            {
+                m_logger.info(String.format("%s tried to hearth but their hearth is on cooldown", playerName));
+
+                PrettyMessages.sendMessage(player, "Your hearth is on cooldown.");
+            }
+            else
+            {
+                final Location bedSpawnLocation = player.getBedSpawnLocation();
+                if (bedSpawnLocation != null)
                 {
-                    m_logger.info(String.format("%s tried to hearth but their hearth is on cooldown", playerName));
-                    PrettyMessages.sendMessage(player, "Your hearth is on cooldown.");
-                }
-                else
-                {
-                    final Location bedSpawnLocation = player.getBedSpawnLocation();
-                    if (bedSpawnLocation != null)
+                    final Location currentLocation = player.getLocation();
+                    final World currentWorld = currentLocation.getWorld();
+                    final World bedSpawnWorld = bedSpawnLocation.getWorld();
+
+                    if (player.teleport(bedSpawnLocation))
                     {
-                        final Location currentLocation = player.getLocation();
-
-                        player.teleport(bedSpawnLocation);
                         m_database.disableHearth(playerIdentity);
-
-                        PrettyMessages.sendMessage(player, "Your hearth is now on cooldown.");
-
                         m_logger.info(
                                 String.format(
                                         "%s hearthed from %s at %d/%d/%d to %s at %d/%d/%d",
                                         playerName,
-                                        currentLocation.getWorld().getName(),
+                                        currentWorld.getName(),
                                         currentLocation.getBlockX(),
                                         currentLocation.getBlockY(),
                                         currentLocation.getBlockZ(),
-                                        bedSpawnLocation.getWorld().getName(),
+                                        bedSpawnWorld.getName(),
                                         bedSpawnLocation.getBlockX(),
                                         bedSpawnLocation.getBlockY(),
                                         bedSpawnLocation.getBlockZ()));
+
+                        PrettyMessages.sendMessage(player, "Your hearth is now on cooldown.");
                     }
                     else
                     {
-                        PrettyMessages.sendMessage(player, "You must have a bed spawn location to hearth.");
-                        m_logger.info(String.format("%s tried to hearth without a bed spawn location", playerName));
+                        m_logger.info(
+                                String.format(
+                                        "%s could not hearth from %s at %d/%d/%d to %s at %d/%d/%d",
+                                        playerName,
+                                        currentWorld.getName(),
+                                        currentLocation.getBlockX(),
+                                        currentLocation.getBlockY(),
+                                        currentLocation.getBlockZ(),
+                                        bedSpawnWorld.getName(),
+                                        bedSpawnLocation.getBlockX(),
+                                        bedSpawnLocation.getBlockY(),
+                                        bedSpawnLocation.getBlockZ()));
+
+                        PrettyMessages.sendMessage(player, "Something preventing you from using your hearth.");
                     }
                 }
+                else
+                {
+                    m_logger.info(String.format("%s tried to hearth without a bed spawn location", playerName));
 
-                return true;
+                    PrettyMessages.sendMessage(player, "You must have a bed spawn location to hearth.");
+                }
+            }
 
-            default:
-                return false;
+            return true;
         }
+
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////
     // Non-Public Field(s).
     ///////////////////////////////////////////////////////////////////////////
 
+    private final static String HEARTH_COMMAND_NAME = "hearth";
     private final HearthDatabase m_database;
     private final Server m_server;
     private final PluginLogger m_logger;
