389422e873ee5730fe93d434f3171c854d896b96
Paul-Andrew Joseph Miseiko <pmiseiko@gmail.com>
2020-12-01 21:45:51 -0500

    Rewrite the hearth implementation.

--- src/main/java/ca/nightfury/minecraft/plugin/player/hearth/HearthDatabaseImpl.java
+++ src/main/java/ca/nightfury/minecraft/plugin/player/hearth/HearthDatabaseImpl.java
@@ -2,14 +2,14 @@ package ca.nightfury.minecraft.plugin.player.hearth;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.bukkit.plugin.PluginLogger;
 import org.mapdb.DB;
-import org.mapdb.DB.HashSetMaker;
 import org.mapdb.DBMaker;
 import org.mapdb.DBMaker.Maker;
+import org.mapdb.Serializer;
 
 import ca.nightfury.minecraft.plugin.database.PlayerIdentity;
 import ca.nightfury.minecraft.plugin.database.PlayerIdentitySerializer;
@@ -29,16 +29,13 @@ public class HearthDatabaseImpl implements HearthDatabase
         dbMaker.fileMmapEnableIfSupported();
 
         m_database = dbMaker.make();
-
-        final HashSetMaker<PlayerIdentity> hearthDisabledMaker =
-                m_database.hashSet("HearthDisabled", PlayerIdentitySerializer.SINGLETON);
-
-        hearthDisabledMaker.expireAfterCreate(1, TimeUnit.DAYS);
-        hearthDisabledMaker.expireAfterCreate();
-
-        m_hearthDisabled = hearthDisabledMaker.createOrOpen();
+        m_hearthCooldownTimeout = m_database.hashMap(
+                "HearthCooldownTimeout",
+                PlayerIdentitySerializer.SINGLETON,
+                Serializer.LONG).createOrOpen();
         m_logger = logger;
-        m_logger.info(String.format("Disabled Hearth(s): %d", m_hearthDisabled.size()));
+        m_logger.info(String.format("Disabled Hearth(s): %d", m_hearthCooldownTimeout.size()));
+        m_logger.info(String.format("Hearth Cooldown Timeout: %d", HEARTH_COOLDOWN_TIMEOUT));
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -48,13 +45,19 @@ public class HearthDatabaseImpl implements HearthDatabase
     @Override
     public void disableHearth(final PlayerIdentity playerIdentity)
     {
-        m_hearthDisabled.add(playerIdentity);
+        m_hearthCooldownTimeout.put(playerIdentity, System.currentTimeMillis() + HEARTH_COOLDOWN_TIMEOUT);
     }
 
     @Override
     public boolean isHearthDisabled(final PlayerIdentity playerIdentity)
     {
-        return m_hearthDisabled.contains(playerIdentity);
+        if (m_hearthCooldownTimeout.containsKey(playerIdentity))
+        {
+            final long timeout = m_hearthCooldownTimeout.get(playerIdentity);
+            return timeout > System.currentTimeMillis();
+        }
+
+        return false;
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -84,7 +87,8 @@ public class HearthDatabaseImpl implements HearthDatabase
     ///////////////////////////////////////////////////////////////////////////
 
     private final static String DATABASE_FILE_NAME = "hearth.db";
+    private final static long HEARTH_COOLDOWN_TIMEOUT = TimeUnit.DAYS.toMillis(1);
     private final DB m_database;
-    private final Set<PlayerIdentity> m_hearthDisabled;
+    private final Map<PlayerIdentity, Long> m_hearthCooldownTimeout;
     private final PluginLogger m_logger;
 }
